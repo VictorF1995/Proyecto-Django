2a52c38076d7461a1b1eb5399d316a429ef9e750
VictorF1995 <victor.espindola.pedidos@gmail.com>
2023-09-12 23:48:20 -0300

    actualizacion

--- proyectodjango/urls.py
+++ proyectodjango/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+
 
 
 urlpatterns = [
-    path('fecha/', views.fecha),
-    path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
-    path('hola/', views.hola),
-    path('mi-template/', views.mi_template),
-    path('mi-template/<str:nombre>', views.tu_template),
-    path('prueba-template/', views.prueba_template),
-    path('ver-personas/', views.ver_persona),
-    path('crear-personas/<str:nombre>/<str:apellido>/', views.crear_persona),
     path('admin/', admin.site.urls),
 ]
